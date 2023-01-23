2d328b8a550850e7aa9e6c2388823c70120db71a
zack v <zvcoding@gmail.com>
2023-01-23 14:27:30 -0500

    with cors corrections

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['97.107.140.69']
 # Application definition
 
 INSTALLED_APPS = [
+    'sketch.apps.SketchConfig',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
